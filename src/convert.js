9d06d2d0b7c6224b7fd7f8dc942cb2c6ff7f8014
Adam Rafiandri <rafiandria23@gmail.com>
2020-03-22 23:07:05 +0700

    - Added interactive description field for every response. - Added execution time.

--- src/convert.js
+++ src/convert.js
@@ -5,6 +5,8 @@ const spinner = require('ora')();
 const readline = require('./readline');
 
 export default () => {
+  spinner.start(`Initializing MD Converter...`);
+
   const configPath = `${process.cwd()}/dokuin.config.json`;
   const endpointsPath = `${process.cwd()}/dokuin.endpoints.json`;
 
@@ -13,65 +15,83 @@ export default () => {
     fs.readFileSync(endpointsPath, { encoding: 'utf8' })
   );
 
-  const dokuinData = { ...config, endpoints };
-
-  spinner.start(`Converting project ${dokuinData.name} endpoints into MD...\n`);
-
-  const convertedEndpoints = endpoints.map(endpoint => {
-    const { method, path, description, headers, body, response } = endpoint;
-
-    const result = [
-      { h2: `**${method.toUpperCase()}** ${path}` },
-      { p: description },
-      { h3: 'Request' },
-      {
-        code: {
-          language: 'javascript',
-          content: JSON.stringify(
-            {
-              method,
-              path,
-              headers,
-              body
-            },
-            null,
-            2
-          )
-        }
-      },
-      { h3: 'Response' },
-      {
-        code: {
-          language: 'javascript',
-          content: JSON.stringify(response, null, 2)
-        }
-      }
-    ];
-
-    return result;
+  const noResponseEndpoints = endpoints.filter(endpoint => {
+    return !endpoint.response || endpoint.errorResponse.length <= 0;
   });
 
-  const convertedData = json2md([
-    { h1: dokuinData.name },
-    { p: dokuinData.description },
-    { blockquote: `by ${dokuinData.author}` },
-    { h4: `Base URL` },
-    {p: config.baseURL },
-    ...convertedEndpoints,
-    {
-      h5: {
-        link: {
-          title: 'Powered by DokuIn',
-          source: 'https://www.npmjs.com/package/dokuinjs'
+  if (noResponseEndpoints.length > 0) {
+    spinner.warn(
+      `No responses to convert, make sure you have run 'dokuin run' to record REST API responses.`
+    );
+    readline.close();
+  } else if (endpoints.length <= 0) {
+    spinner.warn(
+      `There's no endpoints. Run 'dokuin create' to generate endpoints!`
+    );
+    readline.close();
+  } else {
+    const dokuinData = { ...config, endpoints };
+    spinner.succeed(`Successfully initialized MD Converter.`);
+    spinner.start(
+      `Converting project ${dokuinData.name} endpoints into MD...\n`
+    );
+
+    const convertedEndpoints = endpoints.map(endpoint => {
+      const { method, path, description, headers, body, response } = endpoint;
+
+      const result = [
+        { h2: `**${method.toUpperCase()}** ${path}` },
+        { p: description },
+        { h3: 'Request' },
+        {
+          code: {
+            language: 'javascript',
+            content: JSON.stringify(
+              {
+                method,
+                path,
+                headers,
+                body
+              },
+              null,
+              2
+            )
+          }
+        },
+        { h3: 'Response' },
+        {
+          code: {
+            language: 'javascript',
+            content: JSON.stringify(response, null, 2)
+          }
+        }
+      ];
+
+      return result;
+    });
+
+    const convertedData = json2md([
+      { h1: dokuinData.name },
+      { p: dokuinData.description },
+      { blockquote: `by ${dokuinData.author}` },
+      { h4: `Base URL` },
+      { p: config.baseURL },
+      ...convertedEndpoints,
+      {
+        h5: {
+          link: {
+            title: 'Powered by DokuIn',
+            source: 'https://www.npmjs.com/package/dokuinjs'
+          }
         }
       }
-    }
-  ]);
+    ]);
 
-  const docsPath = `${process.cwd()}/${dokuinData.name}.md`;
-  fs.writeFileSync(docsPath, convertedData, { encoding: 'utf8' });
+    const docsPath = `${process.cwd()}/${dokuinData.name}.md`;
+    fs.writeFileSync(docsPath, convertedData, { encoding: 'utf8' });
 
-  spinner.succeed(`Successfully converted into MD!\n`);
+    spinner.succeed(`Successfully converted into MD!\n`);
 
-  readline.close();
+    readline.close();
+  }
 };
--- src/run.js
+++ src/run.js
@@ -2,15 +2,16 @@ const fs = require('fs');
 const axios = require('axios').default;
 const build = require('build-url');
 const spinner = require('ora')();
+const { Input } = require('enquirer');
 
 const readline = require('./readline');
 
 export default async () => {
+  spinner.start(`Initializing REST Client...`);
+  const start = new Date();
+
   const configPath = `${process.cwd()}/dokuin.config.json`;
   const endpointsPath = `${process.cwd()}/dokuin.endpoints.json`;
-
-  fs.accessSync(configPath, fs.constants.R_OK);
-  fs.accessSync(endpointsPath, fs.constants.R_OK);
 
   const { baseURL } = JSON.parse(
     fs.readFileSync(configPath, { encoding: 'utf8' })
@@ -22,58 +23,108 @@ export default async () => {
     })
   );
 
-  for (const endpoint of endpoints) {
-    const builtURL = build(baseURL, {
-      path: typeof endpoint.path !== 'undefined' ? endpoint.path : baseURL,
-      queryParams: typeof endpoint.query !== 'undefined' ? endpoint.query : {}
+  if (!Array.isArray(endpoints)) {
+    spinner.warn(
+      `Cannot find the endpoints array. There's no endpoints. Run 'dokuin create' to generate endpoints!`
+    );
+    readline.close();
+  } else if (endpoints.length <= 0) {
+    spinner.warn(
+      `There's no endpoints. Run 'dokuin create' to generate endpoints!`
+    );
+    readline.close();
+  } else {
+    spinner.succeed(`Successfully initialized REST Client.`);
+
+    endpoints.forEach(endpoint => {
+      endpoint.errorResponse = [];
     });
 
-    spinner.start(
-      `Processing ${endpoint.method.toUpperCase()} from ${builtURL}...\n`
-    );
-
-    shootApi(builtURL, endpoint)
-      .then(response => {
-        const { status, statusText, headers, data } = response;
-        const finalResponse = {
-          status,
-          statusText,
-          headers,
-          body: data
-        };
-        endpoint.response = finalResponse;
-        fs.writeFileSync(endpointsPath, JSON.stringify(endpoints, null, 2), {
-          encoding: 'utf8'
-        });
-
-        spinner.succeed(
-          `Got response with code ${status}: ${statusText} from ${builtURL}\n`
-        );
-      })
-      .catch(err => {
-        const { status, statusText, headers, data } = err.response;
-        const finalErrResponse = {
-          status,
-          statusText,
-          headers,
-          body: data
-        };
-        endpoint.response = finalErrResponse;
-        fs.writeFileSync(endpointsPath, JSON.stringify(endpoints, null, 2), {
-          encoding: 'utf8'
-        });
-
-        spinner.fail(
-          `Got error with code ${status}: ${statusText} from ${builtURL}\n`
-        );
+    for (const endpoint of endpoints) {
+      const builtURL = build(baseURL, {
+        path: typeof endpoint.path !== 'undefined' ? endpoint.path : baseURL,
+        queryParams: typeof endpoint.query !== 'undefined' ? endpoint.query : {}
       });
+
+      try {
+        spinner.start(
+          `Processing ${endpoint.method.toUpperCase()} from ${builtURL}...`
+        );
+
+        const response = await shootApi(builtURL, endpoint);
+
+        const { status, statusText, headers, data } = response;
+          const finalResponse = {
+            status,
+            statusText,
+            headers,
+            body: data
+          };
+
+          endpoint.response = finalResponse;
+
+          spinner.succeed(
+            `Got response with code ${status}: ${statusText} from ${builtURL}`
+          );
+
+          const prompt = new Input({
+            name: 'description',
+            message: `Response description for status code ${status} ${statusText}: `
+          });
+
+          const description = await prompt.run();
+
+          endpoint.response.description = description;
+          fs.writeFileSync(
+            endpointsPath,
+            JSON.stringify(endpoints, null, 2),
+            {
+              encoding: 'utf8'
+            }
+          );
+
+      } catch (err) {
+        const { status, statusText, headers, data } = err.response;
+          const finalErrResponse = {
+            status,
+            statusText,
+            headers,
+            body: data
+          };
+          endpoint.errorResponse.push(finalErrResponse);
+          fs.writeFileSync(endpointsPath, JSON.stringify(endpoints, null, 2), {
+            encoding: 'utf8'
+          });
+
+          spinner.fail(
+            `Got error with code ${status}: ${statusText} from ${builtURL}`
+          );
+
+          const prompt = new Input({
+            name: 'description',
+            message: `Response description for status code ${status} ${statusText}: `
+          });
+
+          const response = await prompt.run();
+
+          finalErrResponse.description = response;
+          endpoint.errorResponse.push(finalErrResponse);
+          fs.writeFileSync(
+            endpointsPath,
+            JSON.stringify(endpoints, null, 2),
+            {
+              encoding: 'utf8'
+            }
+          );
+      }
+    }
+
+    fs.writeFileSync(endpointsPath, JSON.stringify(endpoints, null, 2), {
+      encoding: 'utf8'
+    });
+    console.log(`Finished executing in ${parseFloat((new Date() - start) / 1000).toFixed(2)} seconds.`);
+    readline.close();
   }
-
-  fs.writeFileSync(endpointsPath, JSON.stringify(endpoints, null, 2), {
-    encoding: 'utf8'
-  });
-
-  readline.close();
 };
 
 async function shootApi(url, config) {
