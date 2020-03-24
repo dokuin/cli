1ace70447f77e983100ceaa9d6d26e9063fce632
Arona Nur Tetulis <arona.nur.tetulis@gmail.com>
2020-03-24 09:12:30 +0700

    Add validation into cli

--- src/mutation.js
+++ src/mutation.js
@@ -8,30 +8,32 @@ const { updateList }  = require('./updateList')
 
 export const DeleteEndpoint = () => {
   let rawList = Cli.rawList()
-  if(rawList.length > 0){
-    Cli.list(false)
-    readline.question(`\nEndpoint's ID: `, (id) => {
-      const spinner = ora(`Your endpoint with id ${id} are still being deleted`).start()
-      const newList = rawList.filter(list => list.id != id)
-      fs.writeFile('dokuin.endpoints.json', JSON.stringify(newList, null, 2), (err) => {
-        if(err){
-          console.log(err)
+  if(rawList){
+    if(rawList.length > 0){
+      Cli.list(false)
+      readline.question(`\nEndpoint's ID: `, (id) => {
+        const spinner = ora(`Your endpoint with id ${id} are still being deleted`).start()
+        const newList = rawList.filter(list => list.id != id)
+        fs.writeFile('dokuin.endpoints.json', JSON.stringify(newList, null, 2), (err) => {
+          if(err){
+            console.log(err)
+          }else{
+            spinner.succeed(`Your endpoint with id ${id} have been deleted`)
+            readline.close()
+          }
+        })
+      })
+    }else{
+      console.log(chalk.red(boxen('\nSorry but your endpoint list is empty!', { padding: 1 })))
+      readline.question('Do you want to make endpoint ? (yes or no) ', (answer) => {
+        if(answer === 'yes' || answer === 'y'){
+          console.clear()
+          Cli.create()
         }else{
-          spinner.succeed(`Your endpoint with id ${id} have been deleted`)
           readline.close()
         }
       })
-    })
-  }else{
-    console.log(chalk.red(boxen('Sorry but your endpoint list is empty!', { padding: 1 })))
-    readline.question('Do you want to make endpoint ? (yes or no) ', (answer) => {
-      if(answer === 'yes' || answer === 'y'){
-        console.clear()
-        Cli.create()
-      }else{
-        readline.close()
-      }
-    })
+    }
   }
 }
 
--- src/updateList.js
+++ src/updateList.js
@@ -11,58 +11,67 @@ const warning = chalk.keyword('orange');
 const { updateCommand } = require('./update');
 
 export const updateList = () => {
-  process.stdout.clearLine()
   let rawList = Cli.rawList()
   
-  if(rawList.length > 0){
-    console.log(warning('Which endpoint : \n'))
+  if(rawList){
+    if(rawList.length > 0){
+      console.log(warning('Which endpoint : \n'))
     
-    let selected = 0
+      let selected = 0
+      
+      const renderUpdate = () => {
+        rawList.map((el, i) => {
+          charm2.foreground("cyan")
+          charm2.write('[' + (i === selected ? "X" : " ") + "] ");
+          (i !== selected) && charm2.foreground("white")
+          charm2.write(`method: ${el.method}, path: ${el.path} ` + "\n")
+          charm2.foreground("white")
+        })
+      }
     
-    const renderUpdate = () => {
-      rawList.map((el, i) => {
-        charm2.foreground("cyan")
-        charm2.write('[' + (i === selected ? "X" : " ") + "] ");
-        (i !== selected) && charm2.foreground("white")
-        charm2.write(`method: ${el.method}, path: ${el.path} ` + "\n")
-        charm2.foreground("white")
+      process.stdin.on("keypress", (s, key) => {
+        if(key.name === 'up' && (selected - 1) >= 0){
+          selected--;
+        }else if(key.name === 'down' && (selected + 1) < rawList.length){
+          selected++;
+        }else if(key.name === 'up' && (selected - 1) < 0){
+          selected = rawList.length - 1
+        }else if(key.name === 'down' && (selected + 1) === rawList.length){
+          selected = 0
+        }else{
+          return;
+        }
+        
+        charm2.erase("line")
+        rawList.map(() => {
+          charm2.up(1)
+          charm2.erase("line");
+        })
+        renderUpdate()
+      })
+    
+      renderUpdate()
+      
+      readline.on("line", (line) => {
+        charm2.destroy()
+        if(rawList[selected]){
+          updateCommand(rawList[selected].id)
+        }
+        selected = 100000
+      }).on("close", () => {
+        readline.close();
+        process.exit(0)
+      })
+    }else{
+      console.log(chalk.red(boxen('\nSorry but your endpoint list is empty!', { padding: 1 })))
+      readline.question('Do you want to make endpoint ? (yes or no) ', (answer) => {
+        if(answer === 'yes' || answer === 'y'){
+          console.clear()
+          Cli.create()
+        }else{
+          readline.close()
+        }
       })
     }
-  
-    process.stdin.on("keypress", (s, key) => {
-      if(key.name === 'up' && (selected - 1) >= 0){
-        selected--;
-      }else if(key.name === 'down' && (selected + 1) < rawList.length){
-        selected++;
-      }else if(key.name === 'up' && (selected - 1) < 0){
-        selected = rawList.length - 1
-      }else if(key.name === 'down' && (selected + 1) === rawList.length){
-        selected = 0
-      }else{
-        return;
-      }
-      
-      charm2.erase("line")
-      rawList.map(() => {
-        charm2.up(1)
-        charm2.erase("line");
-      })
-      renderUpdate()
-    })
-  
-    renderUpdate()
-    
-    readline.on("line", (line) => {
-      charm2.destroy()
-      if(rawList[selected]){
-        updateCommand(rawList[selected].id)
-      }
-      selected = 100000
-    }).on("close", () => {
-      readline.close();
-      process.exit(0)
-    })
-  }else{
-    console.log(chalk.red(boxen('Sorry but endpoint doesnt exist!', { padding: 1 })))
   }
 }
